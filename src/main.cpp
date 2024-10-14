fe5981883589257a937296f6810c3e0d17fb3ff1
Oscar Garrido <oscar.garrido.o@ug.uchile.cl>
2024-10-14 18:51:22 -0300

    cambio orden del codigo

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,12 @@
-#include <iostream>
 #include <vector>
 #include <random>
 #include <chrono>
 #include <filesystem>
 #include "cppcsv.h"
+#include "binary_tree.cpp"
+#include "splay_tree.cpp"
 
-std::vector<int> generate_secuence(int n, int min = 0, int max = INT32_MAX) {
+std::vector<int> gen_secuence(int n, int min = 0, int max = INT_MAX) {
     std::vector<int> secuence;
     secuence.reserve(n);
 
@@ -19,39 +20,60 @@ std::vector<int> generate_secuence(int n, int min = 0, int max = INT32_MAX) {
     return secuence;
 }
 
-int main(void){
-    std::vector<int> seq = generate_secuence(pow(2, 24));
+void experiment_1(std::vector<int> n_seq, std::string save_path) {
+    CSVFile bt_csv(save_path + "experiment_1.csv");
+    bt_csv.write_line("i", "BT time(s)", "BT avg time (s)", "ST time(s)", "ST avg time (s)");
+
+    int inserts = 1;
+    int n = 100000;
+    int m = 100 * n;
+    
+    std::vector<int> m_seq;
+    m_seq.reserve(m);
+    
+    auto seq_it = n_seq.begin();
+    NodeBT bin_tree(*seq_it);
+    seq_it++;
+
+    for(; seq_it != n_seq.end(); seq_it++, inserts++) {
+        bin_tree.insert(*seq_it);
+        inserts++;
+        if (inserts == n) {
+            std::cout << "finding m = " << m << "in n = " << n << std::endl;
+
+            m 
+            auto start_time = std::chrono::high_resolution_clock::now();
+            for(auto find_it = seq.begin(); find_it != seq.end(); find_it++, inserts++) {
+                
+            }
+
+            auto current_time = std::chrono::high_resolution_clock::now();
+            std::chrono::duration<float> duration = current_time - start_time;
+            float time_spent = duration.count();
+            float avg_time_spent = time_spent/inserts;
+            csv.write_line(inserts, time_spent, avg_time_spent);
+
+            std::cout << "time spent = " << time_spent << std::endl;
+            std::cout << "avg time per insert = " << avg_time_spent << std::endl;
+            n += 100000;
+        }
+    }
+}
+
+int main(void) {
+    std::vector<int> n_seq = gen_secuence(1000000, INT_MIN);
     std::cout << "Starting tests" << std::endl << std::endl;
 
     std::string save_path = "results/";
     std::filesystem::create_directories(save_path);
 
-    for(int c = 2; c <= 6 ; c++) {
-        int c_max = pow(2, c);
-        std::cout << "Insertion using c_max = " << c_max << std::endl << std::endl;
+    experiment_1(seq, save_path);
 
-        CSVFile csv(save_path + "c_max " + std::to_string(c_max) +".csv");
-        csv.write_line("i", "I/Os","avg I/Os per ins", "time (ms)", "fill %");
+    /*
+    experiment_2(seq, save_path)
+    experiment_3(seq, save_path)
+    experiment_4(seq, save_path)
+    */
 
-        int count = 1;
-        int i = 10; 
-        int n = pow(2, i);
-        
-        auto start_time = std::chrono::high_resolution_clock::now();
-        for(auto it = seq.begin(); it != seq.end(); it++, count++) {
-            if (count == n) {
-                auto current_time = std::chrono::high_resolution_clock::now();
-                std::chrono::duration<float> duration = current_time - start_time;
-                float delta_time = duration.count();
-                csv.write_line(i, delta_time);
-
-                std::cout << "reached = 2^" << i << std::endl;
-                std::cout << "time spent = " << delta_time << std::endl;
-
-                i++;
-                n = pow(2, i);
-            }
-        }
-    }
     std::cout << "End of test" << std::endl;
 }
