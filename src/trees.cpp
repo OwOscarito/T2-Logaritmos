19892604f1d8e3b3b1a0dd9187db9c931633543f
Oscar Garrido <oscar.garrido.o@ug.uchile.cl>
2024-11-08 06:30:17 -0300

    feat: zig, zag, zigzig, zigzag, zagzig,zagzag

--- src/trees.cpp
+++ src/trees.cpp
@@ -156,72 +156,86 @@ class SplayTree {
         if (node == nullptr || node->value == x)
         return;
 
-        if (node->value > x and root->left != nullptr) {
-            // zig zig
-            if (node->left->value > x) {
-                splay(x, node->left->left);
-                // right rotate
-                zig_zig(node->left);
-            }
-            // zig zag
-            else if (node->left->value < x and node->left->right != nullptr) {
-                splay(x, node->left->right);
-                zig_zag(node->left);
-            }
-        } else if (node->right != nullptr){
-            if (node->right->value > x) {
-                splay(x, node->right->left);
-                if (node->right->left != nullptr)
-                    zig(node->right);
-            }
-            else if (node->right->value < x) {
-                splay(x, node->right->right);
-                zag(node);
-            }
-            return (node->right != nullptr) ? node : leftRotate(root);
-        }
     }
-
+    // Zig: y(x(A, B), C) → x(A, y(B, C)) (sólo si y es raíz)
     void zig(TreeNode *y) {
         TreeNode *x = y->left;
-        y->left = x->right;
+        TreeNode *B = x->right;
+
+        y->left = B;
         x->right = y;
+        
         std::swap(y, x);
     }
-
+    // Zag: y(A, x(B, C)) → x(y(A, B), C) (sólo si y es raíz)
     void zag(TreeNode *y) {
         TreeNode *x = y->right;
-        y->right = x->left;
+        TreeNode *B = x->left;
+
+        y->right = B;
         x->left = y;
+        
         std::swap(y, x);
     }
-
+    // Zig-zig: z(y(x(A, B), C), D) → x(A, y(B, z(C, D)))
     void zig_zig(TreeNode *z) {
-        zig(z->left);
-        if (z->left != nullptr) {
-            zig(z);
-        }
-    }
+        TreeNode *y = z->left;
+        TreeNode *x = y->left;
 
+        TreeNode *B = x->right;
+        TreeNode *C = y->right;
+
+        z->left = C;
+        y->left = B;
+        y->right = z;
+        x->right = y;
+
+        std::swap(z, x);
+    }
+    // Zig-zag: z(y(A, x(B, C)), D) → x(y(A, B), z(C, D))
     void zig_zag(TreeNode *z) {
-        zag(z->right);
-        if (z->right != nullptr) {
-            zig(z);
-        }
-    }
+        TreeNode *y = z->left;
+        TreeNode *x = y->right;
 
+        TreeNode *B = x->left;
+        TreeNode *C = x->right;
+
+        x->left = y;
+        x->right = z;
+        y->right = B;
+        z->left = C;
+
+        std::swap(z, x);
+    }
+    // Zag-zig: z(A, y(x(B, C), D)) → x(z(A, B), y(C, D))
     void zag_zig(TreeNode *z) {
-        zig(z->left);
-        if (z->left != nullptr) {
-            zag(z);
-        }
-    }
+        TreeNode *y = z->right;
+        TreeNode *x = y->left;
 
+        TreeNode *B = x->left;
+        TreeNode *C = x->right;
+
+        x->left = z;
+        x->right = y;
+        y->left = C;
+        z->right = B;
+
+        std::swap(z, x);
+    }
+    // Zag-zag: z(A, y(B, x(C, D))) → x(y(z(A, B), C), D)
     void zag_zag(TreeNode *z) {
-        zig(z->left);
-        if (z->left != nullptr) {
-            zig(z);
-        }
+        TreeNode *y = z->right;
+        TreeNode *x = y->left;
+
+        TreeNode *B = y->left;
+        TreeNode *C = x->left;
+
+        x->left = y;
+        y->left = z;
+        y->right = C;
+        z->right = B;
+
+        std::swap(z, x);
     }
 };
 
